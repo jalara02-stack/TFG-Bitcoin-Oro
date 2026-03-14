f28c6ceb7a9af97f6fc8a899f4acc47f7b7628b0
jalara02-stack <jalara02@ucm.es>
2026-03-14 14:45:46 +0100

    Update analisis_tfg.R

--- TFG_BTC_ORO_datos/analisis_tfg.R
+++ TFG_BTC_ORO_datos/analisis_tfg.R
@@ -129,3 +129,97 @@ cat("BTC Fase 3 (ETFs):     ", cor(base_datos_final$BTC[fase3], base_datos_final
 cat("\nORO Fase 1 (Pre-COVID):", cor(base_datos_final$ORO[fase1], base_datos_final$SP500[fase1]), "\n")
 cat("ORO Fase 2 (Pandemia): ", cor(base_datos_final$ORO[fase2], base_datos_final$SP500[fase2]), "\n")
 cat("ORO Fase 3 (ETFs):     ", cor(base_datos_final$ORO[fase3], base_datos_final$SP500[fase3]), "\n")
+
+# ------------------------------------------------------------------------------
+# 7. GENERACIÓN DE GRÁFICOS.
+# ------------------------------------------------------------------------------
+# Configuración inicial para guardar o visualizar
+# (Si deseas guardarlos automáticamente, puedes usar png("nombre.png") antes de cada plot)
+# FIGURA 1: Distribución modelos vs Normal (Campanas de Gauss)
+par(mfrow = c(1, 2)) # Dividir pantalla en 2 columnas
+# Bitcoin
+hist(base_datos_final$BTC, breaks = 50, prob = TRUE, col = "lightblue",
+ main = "A) Bitcoin: Normal vs Empírica", xlab = "Rendimientos", ylab = "Densidad")
+lines(density(base_datos_final$BTC), col = "darkblue", lwd = 2)
+curve(dnorm(x, mean = mean(base_datos_final$BTC), sd = sd(base_datos_final$BTC)),
+ col = "red", lwd = 2, lty = 2, add = TRUE)
+# Oro
+hist(base_datos_final$ORO, breaks = 50, prob = TRUE, col = "navajowhite",
+ main = "B) Oro: Normal vs Empírica", xlab = "Rendimientos", ylab = "Densidad")
+lines(density(base_datos_final$ORO), col = "darkorange", lwd = 2)
+curve(dnorm(x, mean = mean(base_datos_final$ORO), sd = sd(base_datos_final$ORO)),
+ col = "red", lwd = 2, lty = 2, add = TRUE)
+par(mfrow = c(1, 1)) # Restaurar pantalla
+# FIGURA 2: Volatilidad Condicional Estimada GARCH (1,1)
+vol_btc <- sigma(ajuste_garch_btc)
+vol_oro <- sigma(ajuste_garch_oro)
+plot(vol_btc, type = "l", col = "blue", ylab = "Volatilidad (Riesgo Diario)",
+ xlab = "Fecha", main = "Volatilidad Condicional Estimada (GARCH 1,1)",
+ ylim = c(0, max(vol_btc)))
+lines(vol_oro, col = "darkorange", lwd = 2)
+legend("topright", legend = c("Bitcoin", "Oro"), col = c("blue", "darkorange"), lwd = 2)
+# FIGURA 3: Correlación Móvil (Ventana de 60 días)
+cor_movil_btc <- rollapply(base_datos_final, width = 60,
+ FUN = function(x) cor(x[, "BTC"], x[, "SP500"]),
+by.column = FALSE, align = "right")
+cor_movil_oro <- rollapply(base_datos_final, width = 60,
+ FUN = function(x) cor(x[, "ORO"], x[, "SP500"]),
+by.column = FALSE, align = "right")
+plot(cor_movil_btc, type = "l", col = "blue", ylim = c(-1, 1),
+ main = "Correlación Móvil (60 días): BTC y Oro vs S&P 500",
+ ylab = "Nivel de Correlación", xlab = "Fecha")
+lines(cor_movil_oro, col = "darkorange", lwd = 2)
+abline(h = 0, col = "black", lty = 2)
+legend("bottomleft", legend = c("BTC", "Oro"), col = c("blue", "darkorange"), lwd = 2)
+# FIGURA 4: Estudio de Robustez (Ventanas de 30, 60 y 120 días)
+# (Ejemplo simplificado para BTC)
+cor_movil_btc_30 <- rollapply(base_datos_final, width = 30, FUN = function(x) cor(x[, "BTC"],
+x[, "SP500"]), by.column = FALSE, align = "right")
+cor_movil_btc_120 <- rollapply(base_datos_final, width = 120, FUN = function(x) cor(x[, "BTC"],
+x[, "SP500"]), by.column = FALSE, align = "right")
+plot(cor_movil_btc_30, type = "l", col = "lightgray", ylim = c(-0.5, 1),
+ main = "Robustez Bitcoin: Correlación a 30, 60 y 120 días", ylab = "Correlación BTCS&P500")
+lines(cor_movil_btc, col = "red", lwd = 2)
+lines(cor_movil_btc_120, col = "darkgreen", lwd = 2)
+abline(h = 0, lty = 2)
+legend("topleft", legend = c("30 días", "60 días", "120 días"), col = c("lightgray", "red",
+"darkgreen"), lwd = 2)
+# FIGURA 5: Correlación SIN el 5% de peores caídas
+datos_sin_crisis <- base_datos_final[base_datos_final$SP500 > q5, ]
+cor_sin_crisis_btc <- rollapply(datos_sin_crisis, width = 60, FUN = function(x) cor(x[, "BTC"],
+x[, "SP500"]), by.column = FALSE, align = "right")
+cor_sin_crisis_oro <- rollapply(datos_sin_crisis, width = 60, FUN = function(x) cor(x[, "ORO"],
+x[, "SP500"]), by.column = FALSE, align = "right")
+plot(cor_sin_crisis_btc, type = "l", col = "blue", ylim = c(-1, 1),
+ main = "Correlación Móvil (60 días): SIN EL 5% DE PEORES CAÍDAS", ylab = "Correlación")
+lines(cor_sin_crisis_oro, col = "darkorange", lwd = 2)
+abline(h = 0, lty = 2)
+# FIGURA 6: Gráfico de Dispersión frente al Pánico (VIX)
+par(mfrow = c(1, 2))
+# Bitcoin
+plot(as.numeric(base_datos_final$VIX), as.numeric(base_datos_final$BTC), col = "darkblue", pch
+= 20,
+ xlab = "Nivel del Índice VIX (Miedo)", ylab = "Rendimientos Diarios BTC", main = "A)
+Bitcoin frente al Pánico (VIX)")
+abline(v = q95_vix, lty = 2, lwd = 2)
+abline(lm(base_datos_final$BTC ~ base_datos_final$VIX), col = "red", lwd = 2)
+# Oro
+plot(as.numeric(base_datos_final$VIX), as.numeric(base_datos_final$ORO), col = "darkorange",
+pch = 20,
+ xlab = "Nivel del Índice VIX (Miedo)", ylab = "Rendimientos Diarios Oro", main = "B) Oro
+frente al Pánico (VIX)")
+abline(v = q95_vix, lty = 2, lwd = 2)
+abline(lm(base_datos_final$ORO ~ base_datos_final$VIX), col = "red", lwd = 2)
+par(mfrow = c(1, 1))
+# FIGURA 7: Evolución Estructural (Gráfico de Barras)
+cor_fases_btc <- c(0.01, 0.43, 0.32) # Valores extraídos del análisis por subperiodos
+cor_fases_oro <- c(-0.13, 0.18, 0.08)
+datos_barras <- rbind(cor_fases_btc, cor_fases_oro)
+colnames(datos_barras) <- c("Fase 1: Pre-COVID", "Fase 2: Pandemia", "Fase 3: ETFs Wall St")
+barplot(datos_barras, beside = TRUE, col = c("royalblue", "darkorange"),
+ main = "Evolución Estructural de la Correlación frente al S&P 500",
+ ylab = "Coeficiente de Correlación de Pearson", ylim = c(-0.2, 0.5),
+ legend.text = c("Bitcoin (BTC)", "Oro Físico (GLD)"),
+ args.legend = list(x = "topleft"))
+abline(h = 0)
+cat("\n=== GRÁFICOS GENERADOS CON ÉXITO ===\n")
